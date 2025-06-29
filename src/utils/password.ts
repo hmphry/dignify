6058157dfecff9c51341c7f21e7f2cedaab3463b
Emily Humphrey <emily@hmphry.com>
2025-06-29 17:18:52 -0400

    feat: add password rules and validation function

--- src/utils/password.ts
+++ src/utils/password.ts
@@ -1,5 +1,34 @@
 import bcrypt from "bcrypt";
 
+// Password must:
+// - between 15 characters and 64 characters long
+// - contain at least 1 uppercase letter
+// - 1 lowercase letter
+// - 1 symbol
+// - 1 number
+export const passwordRules = [
+    { 
+        test: (password: string) => password.length >= 15 && password.length <= 64, 
+        message: "Password must be between 15 and 64 characters long."
+    },
+    { 
+        test: (password: string) => /[A-Z]/.test(password), 
+        message: "Password must contain at least one uppercase letter."
+    },
+    { 
+        test: (password: string) => /[a-z]/.test(password), 
+        message: "Password must contain at least one lowercase letter."
+    },
+    { 
+        test: (password: string) => /[0-9]/.test(password), 
+        message: "Password must contain at least one number."
+    },
+    { 
+        test: (password: string) => /[^A-Za-z0-9]/.test(password), 
+        message: "Password must contain at least one symbol."
+        }
+];
+
 export async function hashPassword(password: string): Promise<string> {
   const saltRounds = parseInt(process.env.BCRYPT_SALT_ROUNDS || "10", 10);
   return bcrypt.hash(password, saltRounds);
@@ -13,12 +42,7 @@ export async function comparePasswords(
 }
 
 export function isValidPassword(password: string): boolean {
-    // TODO: update password needs:
-    // - between 15 characters and 64 characters long
-    // - contain at least 1 uppercase letter
-    // - 1 lowercase letter
-    // - 1 symbol
-    // - 1 number
-  const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[A-Za-z\d]{15,}$/;
-  return passwordRegex.test(password);
+    const results = passwordRules.map(rule => ({...rule, result: rule.test(password)}));
+    if(!results.every(result => result)) throw [results.filter(result => !result.result)];
+    return true;
 }
